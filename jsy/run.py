5c49d122c3b609c822326dfff015ffefeee7198b
opop20207 <opop202077@gmail.com>
2019-05-15 16:49:00 +0900

    a

---
+++ jsy/run.py
@@ -0,0 +1,19 @@
+from flask import request, Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+@app.route('/<number>')
+def numb(number):
+    return number
+
+@app.route('/info', methods=['POST'])
+def info():
+    error=None
+    myage=request.form['age']
+    return render_template('info.html', age=myage)
+
+if __name__ == '__main__':
+    app.run()
