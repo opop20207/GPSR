d902ea7cf1ac1c6d6fc986acf9ebdab2b12c2db3
opop20207 <opop202077@gmail.com>
2019-05-16 20:42:14 +0900

    5/16 jsy

--- GPSR Web development/GPSR/GPSR/GPSR.py
+++ GPSR Web development/GPSR/GPSR/GPSR.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, session, url_for, redirect, \
-     render_template, abort, g, flash
+from __future__ import with_statement
 import time
+from sqlite3 import dbapi2 as sqlite3
 from hashlib import md5
 from datetime import datetime
-from sqlite3 import dbapi2 as sqlite3
+from contextlib import closing
+from flask import Flask, request, session, url_for, redirect, \
+     render_template, abort, g, flash
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask.templating import render_template
 
 #settings
 DEBUG=True
@@ -23,11 +24,19 @@ def connect_db():
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executablescript(f.read())
+            db.cursor().executescript(f.read())
         db.commit()
         
-def get_user_id(username):
-    chk = g.db.execute('select user_id from user where username = ?', [username]).fetchone()
+
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+               for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
+
+        
+def get_user_num(id):
+    chk = g.db.execute('select user_num from user where user_id = ?', [id]).fetchone()
     if chk is not None:
         return chk[0]
     else:
@@ -53,7 +62,7 @@ def teardown_request(exception):
 
 
 @app.route('/')
-def main():
+def home():
     return render_template('home.html')
 
 @app.route('/register', methods=['GET','POST'])
@@ -65,7 +74,7 @@ def register():
         if not request.form['id']:
             error='You have to enter a id'
         elif not request.form['nickname']:
-            error='You have to enter a username'
+            error='You have to enter a nickname'
         elif not request.form['email']:
             error='You have to enter a emali'
         elif '@' not in request.form['email']:
@@ -76,11 +85,11 @@ def register():
             error='You have to enter a password'
         elif request.form['password']!=request.form['password2']:
             error='Passwords not match'
-        elif get_user_id(request.form['username']) is not None:
-            error='The username is already taken'
+        elif get_user_num(request.form['id']) is not None:
+            error='The id is already taken'
         else:
-            g.db.execute('insert into user(user_id, user_name, user_email, user_pw_hash) values(?, ?, ?, ?)',
-                [request.form['id'], request.form['nickname'], request.form['email'], generate_password_hash(requset.form['password'])])
+            g.db.execute('insert into user(user_id, user_nickname, user_email, user_pw_hash) values(?, ?, ?, ?)',
+                [request.form['id'], request.form['nickname'], request.form['email'], generate_password_hash(request.form['password'])])
             g.db.commit()
             flash('You are successfully registered! you can login now')
             return redirect(url_for('login'))
@@ -93,16 +102,18 @@ def login():
         return redirect(url_for('home'))
     error=None
     if request.method == 'POST':
-        user=g.db.execute('select * from user whrer username = ?', [request.form['username']])
+        user=query_db('select * from user where user_id = ?', [request.form['id']], one=True)
         if user is None:
             error = 'Invalid username'
-        elif not check_password_hash(user['pw_hash'], request.form['password']):
+        elif not check_password_hash(user['user_pw_hash'], 
+                                     request.form['password']):
             error = 'Invalid password'
         else:
-            flash('long in')
+            flash('log in')
             session['user_id']=user['user_id']
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
 if __name__ == '__main__' :
+    init_db()
     app.run()
--- GPSR Web development/GPSR/GPSR/schema.sql
+++ GPSR Web development/GPSR/GPSR/schema.sql
@@ -1,26 +1,26 @@
 create table if not exists user (
-  	user_num integer primary key autoincrement,
-  	user_id string not null,
-  	user_nickname string not null,
-  	user_email string not null,
-  	user_pw_hash string not null
+  user_num integer primary key autoincrement,
+  user_id string not null,
+  user_nickname string not null,
+  user_email string not null,
+  user_pw_hash string not null
 );
 
-create table if not exists problem{
-	problem_num integer primary key autoincrement 
-	problem_name string not null,
-	problem_correct integer,
-	problem_text string not null,
-	problem_pub_data integer
-};
+create table if not exists problem (
+  problem_num integer primary key autoincrement, 
+  problem_name string not null,
+  problem_correct integer,
+  problem_text string not null,
+  problem_pub_data integer
+);
 
 create table if not exists follow (
-  	follow_who_id integer,
-  	follow_whom_id integer
+  follow_who_id integer,
+  follow_whom_id integer
 );
 
-create table if not exists board{ 
-	board_num integer primary key autoincrement,
-	board_name string not null,
-	board_text string not null
-};
+create table if not exists board (
+  board_num integer primary key autoincrement,
+  board_name string not null,
+  board_text string not null
+);
