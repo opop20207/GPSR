5aad0c761af8e94b3896fcf07b48a15f82cf9387
Gnagseo <Gnagseo@DESKTOP-L7A1QBN>
2019-05-25 16:43:19 +0900

    5/25 KGS><

---
+++ GPSR Web development/GPSR/GPSR_KGS/schema.sql
@@ -0,0 +1,25 @@
+create table if not exists user (
+  user_num integer primary key autoincrement,
+  user_id string not null,
+  user_nickname string not null,
+  user_email string not null,
+  user_pw_hash string not null
+);
+
+create table if not exists problem (
+  problem_num integer primary key autoincrement, 
+  problem_name string not null,
+  problem_correct integer,
+  problem_text string not null
+);
+
+create table if not exists follow (
+  follow_who_id integer,
+  follow_whom_id integer
+);
+
+create table if not exists board (
+  board_num integer primary key autoincrement,
+  board_name string not null,
+  board_text string not null
+);
