dc849284b7478de715ad27450b0eb18f94c3cc9e
wcssc <wcssc@DESKTOP-RDFL6P5>
2019-05-27 17:12:16 +0900

    asdf

--- GPSR Web development/GPSR/GPSR_chwoo/GPSR.py
+++ GPSR Web development/GPSR/GPSR_chwoo/GPSR.py
@@ -8,7 +8,6 @@ from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash
 from werkzeug.security import check_password_hash, generate_password_hash
 import os
-import sys
 
 #settings
 DEBUG=True
@@ -19,6 +18,10 @@ SECRET_KEY = 'development key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('GPSR_SETTINGS', silent=True)
+
+######################################################################################################################################################
+#######################################################################################################################################################
+#base function
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
@@ -44,7 +47,9 @@ def get_user_num(id):
         return chk[0]
     else:
         return None
-    
+#######################################################################################################################################################
+#######################################################################################################################################################
+#before, teardown function
 
 @app.before_request
 def before_request():
@@ -60,6 +65,9 @@ def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
+#######################################################################################################################################################
+#######################################################################################################################################################
+#home
 
 @app.route('/')
 def home():
@@ -81,7 +89,7 @@ def register():
         elif not request.form['nickname']:
             error='You have to enter a nickname'
         elif not request.form['email']:
-            error='You have to enter a email'
+            error='You have to enter a emali'
         elif '@' not in request.form['email']:
             error='You have to enter a valid email address'
         elif not request.form['password']:
@@ -125,14 +133,22 @@ def logout():
     session.pop('user_num', None)
     return redirect(url_for('home'))
 
+#######################################################################################################################################################
+#######################################################################################################################################################
+#group
 
 @app.route('/group',methods=['GET','POST'])
 def group():
     return render_template('group.html')
 
+#######################################################################################################################################################
+#######################################################################################################################################################
+#problem
 
 @app.route('/problem',methods=['GET','POST'])
 def problem():
+    if not g.user:
+        return redirect(url_for('home'))
     problem_list=query_db('select * from problem')
     return render_template('/problem/problem.html', problem_list=problem_list)
     
@@ -141,43 +157,106 @@ def problem_view(problem_num):
     problem = query_db('select * from problem where problem_num is ?', [problem_num])
     return render_template('/problem/problem_view.html', problem=problem)
 
-@app.route('/problem/compile', methods=['GET', 'POST'])
-def problem_compile():
-    g.db.execute('insert into answer(answer_who,answer_text,answer_result) values(?, ?, ?)',
-                         [g.user['user_id'],request.form['answer_text'], 0])
-    g.db.commit()
-    answer = query_db('select * from answer where answer_text is ?', [request.form['answer_text']])
-    file=open('test_file.cpp', 'w')
-    a = answer[0]
-    file.write(a['answer_text'])
-    os.system('gcc test_file.cpp -o tf')
-    return render_template('/problem/problem_result.html',answer=answer)
-
-
 @app.route('/problem/<problem_num>/view_io')
 def problem_view_io(problem_num):
     return "hi"
 
+@app.route('/problem/compile', methods=['GET', 'POST'])
+def problem_compile():
+    g.db.execute('insert into answer(answer_problem_num,answer_who,answer_text,answer_result) values(?, ?, ?, ?)',
+                         [request.form['answer_problem_num'],g.user['user_id'],request.form['answer_text'], 0])
+    g.db.commit()
+    temp_answer_num = query_db('select answer_num from answer order by answer_num desc limit ?',[1])
+    print()
+    answer = query_db('select * from answer where answer_num is ?', [temp_answer_num[0]['answer_num']])
+    
+    file=open('test_file.c', 'w')
+    a = answer[0]
+    file.write(a['answer_text'])
+    file.close()
+
+    f = os.system('gcc test_file.c')
+    if f == 0:
+        print("!!!")
+        os.system('a.exe')
+        os.remove('a.exe')
+    #os.remove('test_file.c')
+    
+    return render_template('/problem/problem_result.html',answer=answer)
+
+#######################################################################################################################################################
+#######################################################################################################################################################
+#talk
+
 @app.route('/talk',methods=['GET','POST'])
 def talk():
-    board_list=query_db('select * from board')
-    return render_template('/talk/talk.html', board_list=board_list)
+    if not g.user:
+        return redirect(url_for('home'))
+    talk_list=query_db('select * from board')
+    return render_template('/talk/talk.html', talk_list=talk_list)
 
-@app.route('/talk/<board_name>', methods=['GET', 'POST'])
-def board_view(board_name):
-    board = query_db('select * from problem where board_name is ?', [board_name])
-    return render_template('/board/board_view.html', board=board)
+@app.route('/talk/<board_num>', methods=['GET','POST'])
+def talk_view(board_num):
+    talk = query_db('select * from board where board_num is ?', [board_num])
+    return render_template('/talk/talk_view.html',talk=talk, who_id=g.user['user_id'])
+
+@app.route('/talk/write')
+def talk_write():
+    return render_template('/talk/talk_write.html')
+
+@app.route('/talk/add',methods=['GET','POST'])
+def talk_add():
+    error=None
+    if request.method == 'POST':
+        if not request.form['talk_title']:
+            error="You have to enter a title"
+        elif not request.form['talk_body']:
+            error="You have to enter a body"
+        else:
+            g.db.execute('insert into board (board_name, board_text, board_who) values (?, ?, ?)',
+                         [request.form['talk_title'],request.form['talk_body'],g.user['user_id']])
+            g.db.commit()
+            return redirect(url_for('talk'))
+    return render_template('/talk/talk_write.html', error=error)
+
+@app.route('/talk/delete/<board_num>',methods=['POST','GET'])
+def talk_delete(board_num):
+    g.db.execute('delete from board where board_num = ?', board_num)
+    g.db.commit()
+    return redirect(url_for('talk'))
+
+@app.route('/comment/add',methods=['POST','GET'])
+def comment_add():
+    g.db.execute('insert into comment (board_num, comment_text, comment_who) values (?, ?, ?)',
+                         [request.form['board_num'],request.form['talk_body'],g.user['user_id']])
+    g.db.commit()
+    return redirect(url_for('talk'))
+
+
+#######################################################################################################################################################
+#######################################################################################################################################################
+#admin
 
 @app.route('/geonguprincesssecretroom')
 def admin():
     return render_template('/admin/admin.html')
-
 
 #view user_list
 @app.route('/geonguprincesssecretroom/view_user')
 def admin_view_user():
     return render_template('/admin/admin_view_user.html',users=query_db('''
     select * from user limit?''', [PER_PAGE]))
+    
+@app.route('/geonguprincesssecretroom/view_problem')
+def admin_view_problem():
+    return render_template('/admin/admin_view_problem.html',problems=query_db('''
+    select * from problem limit?''', [PER_PAGE]))
+
+#all of user information
+@app.route('/geonguprincesssecretroom/view_user_info/<user_id>')
+def admin_view_user_info(user_id):
+    user = query_db('select * from user where user_id = ?', [user_id], True)
+    return render_template('/admin/admin_view_user_info.html', user=user)
 
 #user delete
 @app.route('/geonguprincesssecretroom/delete_user/<user_id>')
@@ -186,56 +265,24 @@ def admin_delete_user(user_id):
     g.db.commit()
     return redirect(url_for('admin_view_user'))
 
-#all of user information
-@app.route('/geonguprincesssecretroom/view_info/<user_id>')
-def admin_view_info(user_id):
-    user = query_db('select * from user where user_id = ?', [user_id], True)
-    return render_template('/admin/admin_view_info.html', user=user)
-
-@app.route('/geonguprincesssecretroom/view_problem')
-def admin_view_problem():
-    return render_template('/admin/admin_view_problem.html',problems=query_db('''
-    select * from problem limit?''', [PER_PAGE]))
-    
-@app.route('/geonguprincesssecretroom/view_board')
-def admin_view_board():
-    return render_template('/admin/admin_view_board.html',boards=query_db('''
-    select * from board limit?''', [PER_PAGE]))
-
-@app.route('/geonguprincesssecretroom/add',methods=['POST'])
-def add():
-    error=None
-    if request.method == 'POST':
-        if not request.form['problem_title']:
-            error="You have to enter a title"
-        elif not request.form['problem_body']:
-            error="You have to enter a body"
-        else:
-            g.db.execute('insert into problem(problem_name,problem_correct,problem_text) values(?, ?, ?)',
-                         [request.form['problem_title'], 0, request.form['problem_body']])
-            g.db.commit()
-            return redirect(url_for('admin_view_problem'))
-    return render_template('admin/admin_add_problem.html', error=error)
-
-@app.route('/geonguprincesssecretroom/addB',methods=['POST'])
-def addB():
-    error=None
-    if request.method == 'POST':
-        if not request.form['board_name']:
-            error="You have to enter a board name"
-        else:
-            g.db.execute('insert into board(board_name) values(?)', [request.form['board_name']])
-            g.db.commit()
-            return redirect(url_for('admin_view_board'))
-    return render_template('admin/admin_add_board.html', error=error)
-
 @app.route('/geonguprincesssecretroom/add_problem')
 def admin_add_problem():
     return render_template('/admin/admin_add_problem.html', error=None)
 
-@app.route('/geonguprincesssecretroom/add_board')
-def admin_add_board():
-    return render_template('/admin/admin_add_board.html', error=None)
+@app.route('/geonguprincesssecretroom/add_problem_exe',methods=['POST'])
+def admin_add_problem_exe():
+    error=None
+    if request.method == 'POST':
+        if not request.form['problem_name']:
+            error="You have to enter a name"
+        elif not request.form['problem_text']:
+            error="You have to enter a text"
+        else:
+            g.db.execute('insert into problem(problem_name,problem_correct,problem_text) values(?, ?, ?)',
+                         [request.form['problem_name'], 0, request.form['problem_text']])
+            g.db.commit()
+            return redirect(url_for('admin_view_problem'))
+    return render_template('/admin/admin_add_problem.html', error=error)
 
 @app.route('/geonguprincesssecretroom/delete_problem/<problem_num>')
 def admin_delete_problem(problem_num):
@@ -243,17 +290,13 @@ def admin_delete_problem(problem_num):
     g.db.commit()
     return redirect(url_for('admin_view_problem'))
 
-@app.route('/geonguprincesssecretroom/delete_board/<board_num>')
-def admin_delete_board(board_num):
-    g.db.execute('delete from board where board_num = ?', [board_num])
-    g.db.commit()
-    return redirect(url_for('admin_view_board'))
-
-@app.route('/geonguprincesssecretroom/view_info_more_problem/<problem_num>')
-def admin_view_more_problem(problem_num):
+@app.route('/geonguprincesssecretroom/view_problem_info/<problem_num>')
+def admin_view_problem_info(problem_num):
     problem = query_db('select * from problem where problem_num = ?', [problem_num], True)
-    return render_template('/admin/admin_view_info_problem.html', problem=problem, problem_ret=problem["problem_text"])
+    return render_template('/admin/admin_view_problem_info.html', problem=problem, problem_ret=problem["problem_text"])
 
+#######################################################################################################################################################
+#######################################################################################################################################################
 
 if __name__ == '__main__' :
     init_db()
